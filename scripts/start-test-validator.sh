40580b1d58a079cd3642aa6a2a8a7dbc1704c7c8
RECTOR <rheza10@gmail.com>
2025-10-07 16:33:05 +0700

    fix: update CP-AMM ProgramData address for test validator
    Updated CP-AMM ProgramData account address in both Anchor.toml
    and start-test-validator.sh to ensure proper program cloning
    during local test validator initialization.
    
    Files changed:
    - Anchor.toml: Updated [[test.validator.clone]] address
    - scripts/start-test-validator.sh: Updated CP_AMM_PROGRAM_DATA variable

--- scripts/start-test-validator.sh
+++ scripts/start-test-validator.sh
@@ -32,7 +32,7 @@ HELIUS_RPC="https://devnet.helius-rpc.com/?api-key=142fb48a-aa24-4083-99c8-249df
 
 # CP-AMM (Meteora Constant Product AMM)
 CP_AMM_PROGRAM="cpamdpZCGKUy5JxQXB4dcpGPiikHawvSWAd6mEn1sGG"
-CP_AMM_PROGRAM_DATA="AUh8bm2XsMfex3KjYwnUkeoDjqi4QBbsYGa1v8tJL76ViX"
+CP_AMM_PROGRAM_DATA="AUh8bm2XsMfex3KjYGcM3G4uBqUNSDw6HEhWaWMYnyPH"
 CP_AMM_CONFIG="8CNy9goNQNLM4wtgRw528tUQGMKD3vSuFRZY2gLGLLvF"  # Config account (index 0)
 
 # Streamflow
